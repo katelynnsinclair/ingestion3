499875043c2e67e69537230ab83405436dc89b0c
Scott Williams <scott@dp.la>
2018-12-10 08:59:09 -0500

    Hodgepodge of changes to LC harvester to improve performance. Still not working, current issue is with need to increase spark.kryoserializer.buffer.max value and harvest for 150,000 records takes 42 hours

--- src/main/scala/dpla/ingestion3/harvesters/api/LocHarvester.scala
+++ src/main/scala/dpla/ingestion3/harvesters/api/LocHarvester.scala
@@ -1,19 +1,23 @@
 package dpla.ingestion3.harvesters.api
 
+import java.io.File
 import java.net.URL
 
+import com.databricks.spark.avro._
 import dpla.ingestion3.confs.i3Conf
-import dpla.ingestion3.utils.HttpUtils
+import dpla.ingestion3.harvesters.{AvroHelper, Harvester}
+import dpla.ingestion3.utils.{HttpUtils, Utils}
+import org.apache.avro.file.DataFileWriter
+import org.apache.avro.generic.{GenericData, GenericRecord}
+import org.apache.commons.io.FileUtils
 import org.apache.http.client.utils.URIBuilder
 import org.apache.log4j.Logger
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.json4s.DefaultFormats
 import org.json4s.jackson.JsonMethods._
 
 import scala.collection.mutable.ListBuffer
 import scala.util.{Failure, Success, Try}
 import scala.xml.XML
-import com.databricks.spark.avro._
 
 /**
   * Class for harvesting records from the Library of Congress's API
@@ -25,35 +29,43 @@ import com.databricks.spark.avro._
 class LocHarvester(spark: SparkSession,
                    shortName: String,
                    conf: i3Conf,
-                   harvestLogger: Logger)
-  extends ApiHarvester(spark, shortName, conf, harvestLogger) {
+                   logger: Logger)
+  extends Harvester(spark, shortName, conf, logger) with Serializable {
 
   def mimeType: String = "application_json"
 
-  override protected val queryParams: Map[String, String] = Map(
+  protected val queryParams: Map[String, String] = Map(
     "query" -> conf.harvest.query,
     "c" -> conf.harvest.rows
   ).collect{ case (key, Some(value)) => key -> value } // remove None values
 
+
+  /**
+    * Returns set names from configuration file
+    *
+    * @return Array[String] Collection names
+    * @throws RuntimeException If no sets are defined in configuration file
+    */
+  def getCollections: Array[String] = conf.harvest.setlist
+    .getOrElse(throw new RuntimeException("No sets")).split(",")
+
   /**
     * Entry method for invoking LC harvest
     */
-  override def localHarvest: DataFrame = {
-    implicit val formats = DefaultFormats
+  override def localHarvest(): DataFrame = {
     // Get sets from conf
-    val collections = conf.harvest.setlist
-      .getOrElse(throw new RuntimeException("No sets")).split(",")
+    val collections = getCollections
 
-    var itemUrls: ListBuffer[URL] = new ListBuffer[URL]()
+    var itemUrls: ListBuffer[String] = new ListBuffer[String]()
 
-    collections.foreach( collection => {
+    collections.foreach(collection => {
       // Mutable vars for controlling harvest loop
       var continueHarvest = true
       var page = "1"
 
-      harvestLogger.info(s"Processing sitemaps for collection: $collection")
+      logger.info(s"Processing sitemaps for collection: $collection")
 
-      while(continueHarvest) getSinglePage(page, collection) match {
+      while (continueHarvest) getSinglePage(page, collection) match {
         // Handle errors
         case error: ApiError with ApiResponse =>
           logError(error)
@@ -80,53 +92,67 @@ class LocHarvester(spark: SparkSession,
         }
       }
     })
+
     // Log results of item gathering
-    harvestLogger.info(s"Fetched ${itemUrls.size} urls")
-    if(itemUrls.distinct.size != itemUrls.size)
-      harvestLogger.info(s"${itemUrls.distinct.size} distinct values. " +
-        s"${itemUrls.size-itemUrls.distinct.size} duplicates}")
-    // Fetch items
-    val locFetched = fetchLocRecords(itemUrls.distinct)
+    logger.info(s"Collected ${itemUrls.size} urls from collection pages")
+
+    // Check for and report duplicate item urls in feed
+    if (itemUrls.distinct.size != itemUrls.size)
+      logger.info(s"${itemUrls.distinct.size} distinct values. " +
+        s"${itemUrls.size - itemUrls.distinct.size} duplicates}")
+
+    val parallelUrls = spark.sparkContext.parallelize(itemUrls.distinct)
+
+    val fetchUrl = (url: String) => HttpUtils.makeGetRequest(new URL(url)) match {
+      case Failure(e) =>
+
+      //        saveOutErrors(List(
+      //          ApiError(e.getStackTrace.mkString("\n\t"),
+      //            ApiSource(emptyParams, Some(myDumbUrl), None))
+      //        ))
+
+      case Success(response) =>
+        Try {
+          parse(response)
+        } match {
+          case Success(json) =>
+
+            val recordId = (json \\ "item" \\ "id").toString
+
+            if (recordId.nonEmpty)
+              ApiRecord(recordId, compact(render(json)))
+          //            else
+          //              saveOutErrors(List(
+          //                ApiError("Missing required property 'id'",
+          //                  ApiSource(emptyParams, Some(myDumbUrl), Some(compact(render(json)))))
+          //              ))
+
+          case Failure(parseError) =>
+
+          //            saveOutErrors(List(
+          //              ApiError(parseError.getStackTrace.mkString("\n\t"),
+          //                ApiSource(emptyParams, Some(myDumbUrl), Some(response)))
+          //            ))
+
+        }
+    }
+
+    val afterUrls = parallelUrls.map(fetchUrl)
+
+    logger.info("Fetched items ")
+
+    val locRecords = afterUrls.collect { case a: ApiRecord => a }
+
+    logger.info("Filtered for ApiRecords")
 
     // @see ApiHarvester
-    saveOutAll(locFetched)
+
+    saveOutRecords(locRecords.collect().toList)
+
+    logger.info("Saved out")
 
     // Read harvested data into Spark DataFrame and return.
     spark.read.avro(tmpOutStr)
-  }
-
-  /**
-    * Fetch all items from LC
-    *
-    * @param urls Seq[URL] URLs to fetch
-    * @return List[ApiResponse] List containing ApiErrors and ApiRecords
-    */
-  def fetchLocRecords(urls: Seq[URL]): List[ApiResponse] = {
-    spark.sparkContext.parallelize(urls).map(url => {
-      HttpUtils.makeGetRequest(url) match {
-        case Failure(e) =>
-          val urlString = Try { url.toString } match {
-            case Success(urlStr) => Some(urlStr)
-            case _ => None
-          }
-          ApiError(e.getStackTrace.mkString("\n\t"),
-            ApiSource(Map("" -> ""), urlString, None)  )
-        case Success(response) =>
-          Try { parse(response) } match {
-            case Success(json) =>
-              val recordId = (json \\ "item" \\ "id").toString
-              if(recordId.nonEmpty)
-                ApiRecord(recordId, compact(render(json)))
-              else
-                ApiError("Missing required property 'id'",
-                  ApiSource(Map("" -> ""), Option(url.toString), Option(compact(render(json)))))
-            case Failure(parseError) =>
-              ApiError(parseError.getStackTrace.mkString("\n\t"),
-                ApiSource(Map("" -> ""), Option(url.toString), Option(response))  )
-          }
-
-      }
-    }).collect().toList
   }
 
   /**
@@ -137,8 +163,8 @@ class LocHarvester(spark: SparkSession,
     * @param collection Name of collection
     * @return ApiSource or ApiError
     */
-  private def getSinglePage(sp: String, collection: String): ApiResponse = {
-    val url = buildUrl(queryParams.updated("sp", sp).updated("collection", collection))
+  def getSinglePage(sp: String, collection: String): ApiResponse = {
+    val url = buildCollectionUrl(queryParams.updated("sp", sp).updated("collection", collection))
     HttpUtils.makeGetRequest(url) match {
       case Failure(e) =>
         ApiError(e.toString, ApiSource(queryParams, Some(url.toString)))
@@ -156,7 +182,7 @@ class LocHarvester(spark: SparkSession,
     * @param params URL parameters
     * @return
     */
-  def buildUrl(params: Map[String, String]): URL =
+  def buildCollectionUrl(params: Map[String, String]): URL =
     new URIBuilder()
       .setScheme("http")
       .setHost("www.loc.gov")
@@ -174,7 +200,7 @@ class LocHarvester(spark: SparkSession,
     * @param urlStr String
     * @return URL
     */
-  def buildItemUrl(urlStr: String): URL = {
+  def buildItemUrl(urlStr: String): String = {
     val url = new URL(urlStr)
 
     new URIBuilder()
@@ -185,6 +211,7 @@ class LocHarvester(spark: SparkSession,
       .setParameter("at", "item")
       .build()
       .toURL
+      .toString
   }
 
   /**
@@ -194,11 +221,59 @@ class LocHarvester(spark: SparkSession,
     * @param msg Error message
     */
   def logError(error: ApiError, msg: Option[String] = None): Unit = {
-    harvestLogger.error("%s  %s\n%s\n%s".format(
+    logger.error("%s  %s\n%s\n%s".format(
       msg.getOrElse("URL: "),
       error.errorSource.url.getOrElse("!!! Undefined URL !!!"),
       error.errorSource.queryParams,
       error.message
     ))
   }
+
+
+  val tmpOutStr: String = new File(FileUtils.getTempDirectory, shortName).getAbsolutePath
+
+  // Delete temporary output directory and files if they already exist.
+  Utils.deleteRecursively(new File(tmpOutStr))
+
+  private val avroWriter: DataFileWriter[GenericRecord] =
+    AvroHelper.avroWriter(shortName, tmpOutStr, Harvester.schema)
+
+
+  def getAvroWriter: DataFileWriter[GenericRecord] = avroWriter
+
+
+  /**
+    *
+    * @param docs
+    */
+  def saveOutRecords(docs: List[ApiRecord]) = {
+
+    val avroWriter = getAvroWriter
+
+    // TODO Integrate this with File harvester save out methods
+    docs.foreach(doc => {
+      val startTime = System.currentTimeMillis()
+      val unixEpoch = startTime / 1000L
+
+      val genericRecord = new GenericData.Record(Harvester.schema)
+
+      genericRecord.put("id", doc.id)
+      genericRecord.put("ingestDate", unixEpoch)
+      genericRecord.put("provider", shortName)
+      genericRecord.put("document", doc.document)
+      genericRecord.put("mimetype", mimeType)
+
+      avroWriter.append(genericRecord)
+    })
+  }
+  /**
+    * Writes errors out to log file
+    *
+    * @param errors List[ApiErrors}
+    */
+  def saveOutErrors(errors: List[ApiError]): Unit =
+    errors.foreach(error => {
+      logger.error(s"URL: ${error.errorSource.url.getOrElse("No url")}" +
+        s"\nMessage: ${error.message} \n\n")
+    })
 }
